995280e823411aa9245f6b5a1f5bdbc625f1e676
Matt <50199412+MattHillWakatipu@users.noreply.github.com>
2023-01-23 14:24:41 +1300

    Add recursive version of the fibonacci function

--- chat_gpt.py
+++ chat_gpt.py
@@ -59,6 +59,21 @@ def fib2_round3(n):  # return Fibonacci series up to n
     return result
 
 
+def fib_recursive(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib_recursive(n - 1) + fib_recursive(n - 2)
+
+
+def fib_series_recursive(n):
+    result = []
+    for i in range(n):
+        result.append(fib_recursive(i))
+    return result
+
+
 if __name__ == "__main__":
-    # fib(10)
-    fib_round3(10)
+    print(fib_series_recursive(10))
